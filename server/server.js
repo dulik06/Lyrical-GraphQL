f6def37d8d510bcee03c09c605e8bf16e4676f8b
Alicja <dul.alicja@gmail.com>
2018-01-18 10:12:52 +0100

    First commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://dulik06:Qwerty123@ds247357.mlab.com:47357/graphqllyrical';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
